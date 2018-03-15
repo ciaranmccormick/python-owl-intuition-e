b2bb60bc6bc7a2696883c539ad49cdd498bc2b4f
Ciaran McCormick <ciaranmccormick@gmail.com>
2018-03-15 20:05:04 +0000

    Bump version and release to 0.0.3

--- owlenergy/__init__.py
+++ owlenergy/__init__.py
@@ -3,5 +3,5 @@ __author__ = 'Ciaran McCormick'
 __author_email__ = 'ciaran.mccormick@open.ac.uk'
 __license__ = 'MIT'
 
-__version__ = '0.0.2'
-__release__ = '0.0.2'
+__version__ = '0.0.3'
+__release__ = '0.0.3'
